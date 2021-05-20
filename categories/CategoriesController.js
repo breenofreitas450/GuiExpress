542d2e5654437d1db41fe6776ed3c443c5625df5
breenofreitas450 <79101662+breenofreitas450@users.noreply.github.com>
2021-05-19 22:56:37 -0500

    CRUD Final Categoria
    Envio das ultimas rotas criadas e dos ultimos Forms criados, para a listagem inserção delete e update das Categorias criadas

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -3,31 +3,59 @@ const router = express.Router();
 const Category = require("./CategoryModel");
 const slugify = require("slugify");
 
-router.get("/admin/categories/new", (req,res) =>{
-        res.render('admin/categories/new');
-});
-
 router.get("/admin/categories", (req,res) =>{
         Category.findAll().then(categories =>{
                 res.render("admin/categories/index", {categories:categories});
         })        
 });
 
-router.post("admin/categories/save", (req,res) =>{
+router.get("/admin/categories/edit/:id", (req,res) =>{
+        var id = req.params.id
+        if(isNaN(id)){
+                res.redirect("/admin/categories");
+        }
+        Category.findByPk(id).then(categories =>{
+                if(categories !=undefined){
+                        res.render("admin/categories/edit",{category:categories})
+                }else{
+                        res.redirect("/admin/categories");
+                }
+        }).catch(erro =>{
+                res.redirect("/admin/categories");
+        })
+});
+router.get("/admin/categories/new", (req,res) =>{
+        res.render('admin/categories/new');
+});
+
+router.post("/admin/categories/save", (req,res) =>{
         var title = req.body.title;
         if(title != undefined){
                 Category.create({
                     title: title,
                     slug: slugify(title)
                 }).then(() =>{
-                        res.redirect("/");
+                        res.redirect("/admin/categories");
                 })
         }else{
              res.redirect("/categories/admin/new");
         }
 })
 
-router.post("admin/categories/delete", (req,res) =>{
+router.post("/admin/categories/edit", (req,res) =>{
+        var id = req.body.id;
+        var title = req.body.title;
+        Category.update({title: title, slug:slugify(title)}, {
+                where: {
+                         id:id
+                }
+        }).then(() =>{
+                res.redirect("/admin/categories");     
+        })   
+
+});
+
+router.post("/admin/categories/delete", (req,res) =>{
         var id = req.body.id;
         if(id != undefined){
                 if(!isNaN(id)){                        
