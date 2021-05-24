42f9c2ce6f6d8d5e28f7122fc505f8d75084a64b
breenofreitas450 <79101662+breenofreitas450@users.noreply.github.com>
2021-05-24 11:33:04 -0500

    Rota de Criação de artigos
    Rota para criação de artigos Back e Front

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/CategoryModel");
 
 router.get("/articles", (req,res) =>{
     res.send("ROTA DE Artigos");
+});
+
+router.get("/admin/articles/new", (req,res) =>{
+    res.render('admin/articles/new');
 });
 
 module.exports = router;
